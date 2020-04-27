f182bb63374cfbcb1f121c18a08e94d4fbc28ed4
Maura Parker <mauraparker02@gmail.com>
2020-04-26 20:13:36 -0400

    creating constructors for engineer, intern, manager, testing if app runs

--- app.js
+++ app.js
@@ -1,18 +1,155 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+//require all other javascript child extended constructors from Employee
 const inquirer = require("inquirer");
-const path = require("path");
+const render = require("./htmlRenderer.js");
+const Engineer = require("./Engineer");
+const Manager = require("./Manager");
+const Intern = require("./Intern");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//Push objects created with imported Employee constructor class to get object of employee
+var employeeArr = []
 
-const render = require("./lib/htmlRenderer");
 
-    
-//in the inquier you ask for 
+function start(){
+inquirer.prompt(
+        
+        {
+            type: "list",
+            message: "Would you like to create a new employee?",
+            name:"role",
+            choices: ["Manager", "Engineer", "Intern", "Done"]
+        }
+    )
 
+    .then(function(response) {
+        console.log(response)
+       if(response.role === "Engineer"){
+        console.log("Readying Engineer Profile...");
+        createEngineer()
+       }else if(response.role === "Done"){
+           console.log("rendering")
+           render(employeeArr)
+            fs.writeFileSync("../index.html", render(employeeArr), "utf-8");
+       }
+       if(response.role === "Manager"){
+        console.log("Readying Manager Profile...");
+        createManager()
+       }else if(response.role === "Done"){
+           console.log("rendering")
+           render(employeeArr)
+            fs.writeFileSync("../index.html", render(employeeArr), "utf-8");
+       }
+       if(response.role === "Intern"){
+        console.log("Readying Intern Profile...");
+        createIntern()
+       }else if(response.role === "Done"){
+           console.log("rendering")
+           render(employeeArr)
+            fs.writeFileSync("../index.html", render(employeeArr), "utf-8");
+       }
+    });
+} 
+
+    function createEngineer(){
+        inquirer.prompt([
+            {
+            type: "input",
+            message: "What's your name?",
+            name:"name" 
+            },
+            {
+            type: "input",
+            message: "What's your id?",
+            name:"id" 
+            },
+            {
+            type: "input",
+            message: "What's your email?",
+            name:"email" 
+            },
+            {
+            type: "input",
+            message: "What's your username?",
+            name:"username" 
+            }  
+        ]).then(employee=>{
+        newEmployee = new Engineer(employee.name, employee.id, employee.email, employee.username);
+        console.log("New Employee", newEmployee);
+        employeeArr.push(newEmployee);
+        console.log(employeeArr);
+     
+        }).then(()=>{
+           start() 
+        })
+    }
+
+    function createManager(){
+        inquirer.prompt([
+            {
+            type: "input",
+            message: "What's your name?",
+            name:"name" 
+            },
+            {
+            type: "input",
+            message: "What's your id?",
+            name:"id" 
+            },
+            {
+            type: "input",
+            message: "What's your email?",
+            name:"email" 
+            },
+            {
+            type: "input",
+            message: "What's your office?",
+            name:"office" 
+            }
+        ]).then(employee=>{
+        newEmployee = new Manager(employee.name, employee.id, employee.email, employee.office);
+        console.log("New Employee", newEmployee);
+        employeeArr.push(newEmployee);
+        console.log(employeeArr);
+     
+        }).then(()=>{
+           start() 
+        })
+    }
+
+    function createIntern(){
+        inquirer.prompt([
+            {
+            type: "input",
+            message: "What's your name?",
+            name:"name" 
+            },
+            {
+            type: "input",
+            message: "What's your id?",
+            name:"id" 
+            },
+            {
+            type: "input",
+            message: "What's your email?",
+            name:"email" 
+            },
+            {
+            type: "input",
+            message: "What's your school?",
+            name:"school" 
+            }
+        ]).then(employee=>{
+        newEmployee = new Intern(employee.name, employee.id, employee.email, employee.school);
+        console.log("New Employee", newEmployee);
+        employeeArr.push(newEmployee);
+        console.log(employeeArr);
+     
+        }).then(()=>{
+           start() 
+        })
+    }
+
+    start();
 
 
 
